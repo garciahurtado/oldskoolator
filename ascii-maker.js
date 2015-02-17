3374c548ff82c53a2b1679b05c13a902e17b5aca
Garcia Hurtado <ghurtado@gmail.com>
2015-02-16 22:06:44 -0500

    Added character comparison function (Working)

--- ascii-maker.js
+++ ascii-maker.js
@@ -43,7 +43,7 @@ function start(stage) {
 	print("Java version : " + System.getProperty("java.version"));
 
     var root = new StackPane();
-    stage.scene = new Scene(root, 600, 670);
+    stage.scene = new Scene(root, 520, 670);
 
     // Load FX CSS file
     var cssFile = new File(__DIR__ + "style.css");
@@ -163,21 +163,50 @@ function start(stage) {
  * Takes a block of pixels as input and returns the best matching ASCII character in the existing charset
  */
 function convertPixelBlock(inputBlock, width, height){
-    var pixelBlock = downSample(inputBlock, width, height, 8, 4);
-    return pixelBlock;
+    var pixelBlock = pixelize(inputBlock, width, height, 4, 4);
+    var matchingBlock = inputBlock;
+
+    for each(var oneChar in charImages){
+        if(pixelBlock.compareTo(oneChar) == 0) { // equal
+            matchingBlock = oneChar;
+        }
+    }
+
+    return matchingBlock;
 }
 
 /**
  * Downsamples an image (pixel array) using nearest neighbor sampling.
  *
+ * It copies one pixel at a time from the original image, skipping every "sampleLevel" pixels, 
+ * which are treated as "filler" pixels and colored according to its nearest neighbor sampled pixel.
+ *
+ * ex:
+ * sampleLevel 2
+ *
+ * @#@#@#@#
+ * ########
+ * @#@#@#@#
+ * ########
+ *
+ * sampleLevel 3
+ *
+ * @##@##@##
+ * #########
+ * #########
+ * @##@##@##
+ *
+ * @: sampled
+ * #: filler
+ * 
+ * @inputPixels: ByteBuffer with the original pixel data
+ * @width / height: dimensions of the original image
+ * @sampleLevel: how many pixels to skip between samples. The higher this number, the more pixelated the 
+ * resulting image
  * @pixelSize : size of one individual pixel (in bytes)
  */
-function downSample(inputPixels, width, height, sampleLevel, pixelSize){
+function pixelize(inputPixels, width, height, sampleLevel, pixelSize){
     var newPixels = ByteBuffer.allocate(width * height * pixelSize); // 4 = RGBA format
-    // inputPixels.rewind();
-
-    // One pixel at a time, copy from original image scanning rows and colums of the block,
-    // and skipping every "sampleLevel" pixels, to create a downsampled grid of pixels
     var inputPixel;
     var newPixel;
     var inputRow = [];
@@ -193,7 +222,7 @@ function downSample(inputPixels, width, height, sampleLevel, pixelSize){
                     if(x == 0){
                         inputRow = [];
                     }
-                    inputPixel = inputPixels.getInt(index * pixelSize);
+                    inputPixel = clampColor(inputPixels.getInt(index * pixelSize), 50);
                     newPixel = inputPixel;
                     inputRow.push(newPixel);
                 } else {
@@ -207,9 +236,34 @@ function downSample(inputPixels, width, height, sampleLevel, pixelSize){
 
         }
     };
-    newPixels.flip();
+
+    // since we've been writing to this buffer, flip it before returning it
+    newPixels.flip(); 
 
     return newPixels;
+}
+
+/**
+ * "Clamps" the value of an BGRA pixel (provided as an int32) to one of either complete white or 
+ * complete black.
+ *
+ * Uses the formula for luminance: Y = (0.2126*R + 0.7152*G + 0.0722*B)
+ *
+ * @param pixel: Pixel data as an int32, in BGRA format (blue, green, red, alpha)
+ * @param threshold: (0-255) If the total luminance is under this threshold, it will get rounded down to
+ * black, otherwise, white is returned.
+ */
+function clampColor(pixel, threshold){
+    var blue = (pixel & 0xFF000000) >> 24;
+    var green = (pixel & 0x00FF0000) >> 16;
+    var red = (pixel & 0x0000FF00) >> 8;
+    var luminance = (0.2126*red + 0.7152*green + 0.0722*blue);
+
+    if(luminance > threshold){
+        return 0xFFFFFFFF; // white
+    } else {
+        return 0x000000FF; // black
+    }
 }
 
 /**
@@ -232,20 +286,22 @@ function loadCharSets(ctx) {
 
         // For each character in charset map
         for (var x = 0; x < image.width / charWidth; x++) {
-            var charPixels = ByteBuffer.allocate(charWidth * charHeight * 4); // byte buffer to store pixels for new character
-            pixels.getPixels(x * charWidth, 0, charWidth, charHeight, format, charPixels, imageStride);
+            var newCharPixels = ByteBuffer.allocate(charWidth * charHeight * 4); 
+            pixels.getPixels(x * charWidth, 0, charWidth, charHeight, format, newCharPixels, imageStride);
 
             var oneByte;
-            charImages.push(charPixels); 
+            charImages.push(newCharPixels); 
         }
 
         var x = 0;
         var charsetPreviewImage = new WritableImage(500, 500);
         var imageWriter = charsetPreviewImage.getPixelWriter();
 
+        // Display the individual characters of the charmap on the screen
         for each(character in charImages){
+            print("Character: " + character.toString());
             imageWriter.setPixels(x, 0, charWidth, charHeight, format, character, imageStride);
-            x += 16;
+            x += 16; // leave 8px padding between characters
         }
 
         ctx.drawImage(charsetPreviewImage, 8, 8);
